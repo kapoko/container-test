beba6e60899ee02f6902004e7c339b051fdf0b61
kapoko <17455539+kapoko@users.noreply.github.com>
2024-07-23 17:48:04 +0200

    Set ssh port to env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@
 # generate host keys if not present
 ssh-keygen -A
 
-echo $TEST_ENV
+if [ ${SSH_PORT:-22} != 22 ]
+then
+  sed -i s/#Port.*/Port\ $SSH_PORT/ /etc/ssh/sshd_config
+fi
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
